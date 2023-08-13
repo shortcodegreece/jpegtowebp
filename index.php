940fcf7883f7f54ccfce44979dfe0e48d47a9ace
ShortcodeGreece <142135708+shortcodegreece@users.noreply.github.com>
2023-08-13 09:35:02 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Image to WebP Converter</title>
+</head>
+<body>
+  <h1>Image to WebP Converter</h1>
+  
+  <form action="convert.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="image">
+    <button type="submit" name="submit">Convert to WebP</button>
+  </form>
+</body>
+</html>
